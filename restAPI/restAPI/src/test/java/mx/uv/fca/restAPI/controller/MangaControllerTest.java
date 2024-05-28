ec52107427348621bb337c681ee6a3590fda1b3c
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2024-05-28 16:48:29 -0600

    Add Test files

---
+++ restAPI/restAPI/src/test/java/mx/uv/fca/restAPI/controller/MangaControllerTest.java
@@ -0,0 +1,55 @@
+package mx.uv.fca.restAPI.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class MangaControllerTest {
+    @Test
+    void testAddManga() {
+
+    }
+
+    @Test
+    void testDeleteChapter() {
+
+    }
+
+    @Test
+    void testGetChapter() {
+
+    }
+
+    @Test
+    void testGetMangaById() {
+
+    }
+
+    @Test
+    void testGetMangaByTitle() {
+
+    }
+
+    @Test
+    void testGetMangaChapters() {
+
+    }
+
+    @Test
+    void testGetMangas() {
+
+    }
+
+    @Test
+    void testPostChapter() {
+
+    }
+
+    @Test
+    void testUpdateChapter() {
+
+    }
+
+    @Test
+    void testUpdateManga() {
+
+    }
+}
